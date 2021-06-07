e34ac1ce10fbb4646266bb7924b26078ae43f48c
Vikki <v.gnanasambandapillai@garvan.org.au>
2021-06-07 12:34:42 +1000

    Update c, test.sh and add README file and clang-format configuration files

---
+++ README.md
@@ -0,0 +1,16 @@
+A simple program which explains the memory allocation, update and usage in different functions.
+
+- test.sh script compiles the code and checks the memory leaks using valgrind.
+
+- Makefile compiles the code with -g option for valgrind
+
+- pointer.c
+1. allocates memory in a function
+  - Memory allocation function must have pointer of pointer argument.
+
+2. updates the memory in another function
+
+3. access the memory in third function
+
+
+- memory update and access functions should have pointer argument
--- pointer.c
+++ pointer.c
@@ -1,19 +1,73 @@
-#include<stdlib.h>
-#include<stdio.h>
+/*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*/
+//         File: pointer.c
+//               Explains memory allocation, update and usage are done by
+//               separate functions.
+//   Created by: vikgna
+//   Created on: 2021/06/07
+//  Modified by: vikgna
+//  Modified on: 2021/06/07
+//      Version: 1.0.0
+//-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*++++++++++++++++++++++++++++++++ PRIVATE Function ++++++++++++++++++++++++/
+//     Function: allocate_memory ()
+//  Description: Allocates heap for 10 integers and update the dereference of argument
+//               with allocated memory address.
+//   Parameters: pointer of pointer of memory.
+//               Double pointer is used because pass by reference of a pointer
+//      Return : 0 - if memory allocation succeeds
+//               1 - if memory allocation fails
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+int allocate_memory(int** ptr_ptr_memory) {
+  *ptr_ptr_memory = (int*)malloc(10 * sizeof(int));
+  if (*ptr_ptr_memory == NULL) {
+    printf("memory allocation fails");
+    return 1;
+  }
+  return 0;
+}
+
+/*++++++++++++++++++++++++++++++++ PRIVATE Function ++++++++++++++++++++++++/
+//     Function: update_memory ()
+//  Description: Update the memory in heap
+//   Parameters: pointer of memory
+//               Single pointer is used (pass by reference)
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+void update_memory(int* ptr_memory) {
+  for (int i = 0; i < 10; i++) {
+    *(ptr_memory + i) = i;
+  }
+}
+
+/*++++++++++++++++++++++++++++++++ PRIVATE Function ++++++++++++++++++++++++/
+//     Function: use_memory ()
+//  Description: prints values in the heap
+//   Parameters: constant pointer of memory
+//               const is use to prevent the memory from modifying accidentally
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+void use_memory(int* const ptr_memory) {
+  for (int i = 0; i < 10; i++) {
+    printf("%d\n", *(ptr_memory + i));
+  }
+}
 
 int main() {
-  int* p = NULL;
-  p = (int*) malloc(10 * sizeof(int));
+  /* pointer which points the allocated memory */
+  int* ptr_memory = NULL;
+  /* pointer which points the pointer of allocated memory */
+  int** ptr_ptr_memory = &ptr_memory;
 
-  for(int i = 0; i < 10; i++) {
-    *(p + i) = i;
-  }
+  allocate_memory(ptr_ptr_memory);
 
-  for(int i = 0; i < 10; i++) {
-    printf("%d\n",*(p + i));
-  }
+  update_memory(ptr_memory);
 
-  free(p);
-  p = NULL;
+  use_memory(ptr_memory);
+
+  free(ptr_memory);
+  ptr_memory = NULL;
+
   return 1;
 }
